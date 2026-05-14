544e8f667ef9b80ec34abf12546b4d95de21597a
HONGSOKHEARCAMBODIA <emmma7096@gmail.com>
2026-05-14 12:19:15 +0700

    update

--- src/services/loan.js
+++ src/services/loan.js
@@ -4,6 +4,13 @@ export function fetchloan(params = {}){
     return api.get(`/view.loan`,{params})
 }
 
-export function createloan(formData){
-    return api.post(`add.loan`,formData)
+export function createloan(data) {
+  const form = new FormData();
+  form.append('employee_id',     data.employee_id);
+  form.append('loan_amount',     data.loan_amount);
+  form.append('currency_id',     data.currency_id);
+  form.append('loan_start_date', data.loan_start_date);
+  form.append('loan_purpose',    data.loan_purpose || '');
+  form.append('loan_duration',   data.loan_duration);
+  return api.post('/add.loan', form); 
 }
